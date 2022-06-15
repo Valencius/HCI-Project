d777d586710c697823bd2c9d855189b3dd2f2ba1
Immanuel Sitepu <immanuel.sitepu001@binus.ac.id>
2022-06-16 00:25:25 +0700

    Push Progress

---
+++ JavaScript/script.js
@@ -0,0 +1,13 @@
+var menuList = document.getElementById("navbar-main");
+
+menuList.style.maxHeight = "0px";
+function Nav() {
+    if (menuList.style.maxHeight == "0px") {
+        menuList.style.maxHeight = "200px";
+    } else {
+        menuList.style.maxHeight = "0px";
+    }
+}
+
+var navbar = document.querySelector('nav')
+
